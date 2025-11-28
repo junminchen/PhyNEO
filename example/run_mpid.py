1fe8ac1d81086b0853f9f5ba3b895ccf15b10d06
Junmin Chen <72368464+junminchen@users.noreply.github.com>
2025-11-28 16:54:51 +0800

    Add run_mpid.py for OpenMM simulation setup
    This script sets up a molecular dynamics simulation using OpenMM, defining force groups and calculating energy decomposition.

---
+++ example/run_mpid.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+import sys
+from openmm import *
+from openmm.app import *
+from openmm.unit import *
+import mpidplugin
+
+
+def forcegroupify(system):
+    forcegroups = {}
+    for i in range(system.getNumForces()):
+        force = system.getForce(i)
+        force.setForceGroup(i)
+        forcegroups[force] = i
+    return forcegroups
+
+def getEnergyDecomposition(context, forcegroups):
+    energies = {}
+    for f, i in forcegroups.items():
+        energies[f] = context.getState(getEnergy=True, groups=2**i).getPotentialEnergy()
+    return energies
+
+if __name__ == '__main__':
+    pdb_file = 'peo3.pdb'
+    pdb_file = 'EC.pdb'
+    # pdb_file = sys.argv[1]
+    # ff_file = sys.argv[2]
+
+    print("MM Reference Energy:")
+    ff = ForceField('caff_5_mpid_slater_bond.xml')
+
+    # ff = ForceField('opls_solvent.xml','opls_salt.xml')
+    # MM Reference Energy:
+    # TotalEnergy 12038.654780969031 kJ/mol
+    # HarmonicBondForce 6805.629965834548 kJ/mol
+    # PeriodicTorsionForce 10426.308753081981 kJ/mol
+    # NonbondedForce -26223.99837035884 kJ/mol
+    # CustomBondForce 1716.0950505012083 kJ/mol
+    # CustomAngleForce 0.0 kJ/mol
+    # CustomTorsionForce 0.0 kJ/mol
+    # CustomTorsionForce 0.0 kJ/mol
+    # CustomTorsionForce 0.0 kJ/mol
+    # CMMotionRemover 0.0 kJ/mol
+    # HarmonicAngleForce 19314.619381910135 kJ/mol
+
+    # ff = ForceField('merged_forcefield.xml')
+
+    pdb = PDBFile(pdb_file)
+    rc = 8
+    # system = ff.createSystem(pdb.topology, nonbondedCutoff=rc*angstrom, nonbondedMethod=PME, defaultTholeWidth=8)
+    system = ff.createSystem(pdb.topology, nonbondedCutoff=rc*angstrom, nonbondedMethod=PME)
+
+    # system = ff.createSystem(pdb.topology, nonbondedCutoff=rc*angstrom, nonbondedMethod=PME, constraints=HBonds)
+    # system = ff.createSystem(pdb.topology, nonbondedCutoff=rc*unit.angstrom, nonbondedMethod=app.CutoffPeriodic)
+    # system = ff.createSystem(pdb.topology, nonbondedMethod=NoCutoff, constraints=None, removeCMMotion=False)
+
+    # try:
+    #     myplatform = Platform.getPlatformByName('CUDA')
+    #     # Figure out which GPU to run on, i.e. did the user tell us?
+    #     deviceid = argv[1] if len(argv) > 1 else '0'
+    #     myproperties = {'DeviceIndex': deviceid, 'Precision': 'mixed'}
+    #     myproperties = {'DeviceIndex': deviceid, 'Precision': 'double'}
+    # except:
+    #     print("CUDA NOT FOUND!!!!!!!!!!")
+    #     myplatform = None
+    #     deviceid = "N/A"
+
+    # if myplatform:
+    #     simulation = Simulation(modeller.topology, system, integrator, myplatform, myproperties)
+    # else:
+    #     simulation = Simulation(modeller.topology, system, integrator)
+
+    # context = simulation.context
+    # if pdb.topology.getPeriodicBoxVectors():
+    #     context.setPeriodicBoxVectors(*pdb.topology.getPeriodicBoxVectors())
+
+    # print("Running on ", context.getPlatform().getName(), " Device ID:", deviceid)
+
+    forcegroups = forcegroupify(system)
+    integrator = VerletIntegrator(0.1)
+    # platform = Platform.getPlatformByName('CUDA')
+    platform = Platform.getPlatformByName('Reference')
+
+    context = Context(system, integrator, platform)
+    
+    context.setPositions(pdb.positions)
+    state = context.getState(getEnergy=True,getForces=True)
+    energy = state.getPotentialEnergy()
+    forces = state.getForces()
+
+    # # Set distance cutoffs, constraints, and other force-specific options for each
+    # # force we might encounter
+    # forces = {system.getForce(index).__class__.__name__: system.getForce(
+    #     index) for index in range(system.getNumForces())}
+    # # nonbonded_force = forces['NonbondedForce'] 
+    # # two_body_cutoff = nonbonded_force.getCutoffDistance()
+    # two_body_cutoff = 0.8*nanometers
+    # for force in system.getForces():
+    #     if isinstance(force, CustomHbondForce):
+    #         force.setNonbondedMethod(CustomHbondForce.CutoffPeriodic)
+    #         force.setCutoffDistance(two_body_cutoff)
+    #     elif isinstance(force, CustomNonbondedForce):
+    #         force.setNonbondedMethod(CustomNonbondedForce.CutoffPeriodic)
+    #         force.setCutoffDistance(two_body_cutoff)
+    #         force.setUseLongRangeCorrection(True)
+    #     elif isinstance(force, AmoebaMultipoleForce):
+    #         force.setNonbondedMethod(AmoebaMultipoleForce.PME)
+    #     elif isinstance(force, NonbondedForce):
+    #         force.setNonbondedMethod(NonbondedForce.LJPME)
+    #         force.setCutoffDistance(two_body_cutoff)
+    #     elif isinstance(force, CustomManyParticleForce):
+    #         force.setNonbondedMethod(CustomManyParticleForce.CutoffPeriodic)
+    #         force.setCutoffDistance(three_body_cutoff)
+    #     else:
+    #         pass
+
+    # print(force)
+    energies = getEnergyDecomposition(context, forcegroups)
+    print('TotalEnergy', energy)
+    for key in energies.keys():
+        print(key.getName(), energies[key])
+        
+    # import numpy as np
+    # from run_openmm import *    
+    # calc = nb_ff_calculator(pdb_file, 'forcefield_amoeba.xml', 'params_sr.dat')
+    # pdb = app.PDBFile(pdb_file)
+    # pos = np.array(pdb.positions._value) * 10
+    # E_nb, E_nb_sr, E_nb_lr = calc.get_energy(pos)
+    # # print("E_nb:",E_nb) 
+    # # print("E_nb_lr:",E_nb_lr) 
+    # print("E_nb_sr:",E_nb_sr) 
